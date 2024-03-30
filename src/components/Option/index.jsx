bcdb07d1832da4bf68a6c2f05fb37fad7a1e9651
daryazhur <daryazhur@tut.by>
2024-03-30 17:31:55 +0100

    reseni

--- src/components/Option/index.jsx
+++ src/components/Option/index.jsx
@@ -1,11 +1,21 @@
 import { Icon } from '../Icon/index';
 import './style.css';
 
-export const Option = (props) => {
+
+export const Option = ({type, text, onSelected}) => {
+
+  // const handleClickOption = () => {
+  //   onSelected(text)
+  // }
+
+  	const handleClickOption = () => {
+      onSelected(type)
+    }
+
   return (
-    <div className="option">
-      <Icon type={props.type} />
-      <div>{props.text}</div>
+    <div className="option" onClick={handleClickOption}>
+      <Icon type={type} />
+      <div>{text}</div>
     </div>
   );
 };
--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,21 +1,51 @@
 import { QuestionBody } from '../QuestionBody/index';
 import { Option } from '../Option/index';
 import './style.css';
+import {useState} from "react"
 
-export const Question = (props) => {
+export const Question = ({text}) => {
+  
+  /*
+  const [answered, setAnswered] = useState(false)
+
+  const handleSelect = () => {
+    setAnswered(true)
+  }
+
+  return (
+   <div className="question">
+     <QuestionBody 
+       iconType={answered ? "symbolTick" : "symbolQuestion" }
+       text={text}
+     />
+      <div className="question__options">
+        <Option type="smileyStrongYes" text="Souhlasím" onSelected={handleSelect}/>
+        <Option type="smileyYes" text="Spíše souhlasím" onSelected={handleSelect}/>
+        <Option type="smileyNeutral" text="Nevím" onSelected={handleSelect}/>
+        <Option type="smileyNo" text="Spíše nesouhlasím" onSelected={handleSelect}/>
+        <Option type="smileyStrongNo" text="Nesouhlasím" onSelected={handleSelect}/>
+      </div>
+    </div>
+  ); */
+
+  const [answered, setAnswered] = useState("symbolQuestion")
+  const handleSelect = (iconType) => {
+    setAnswered(iconType)
+  }
+
   return (
     <div className="question">
       <QuestionBody 
-        iconType="symbolQuestion" 
-        text={props.text}
+        iconType={answered}
+        text={text}
       />
       <div className="question__options">
-        <Option type="smileyStrongYes" text="Souhlasím"/>
-        <Option type="smileyYes" text="Spíše souhlasím"/>
-        <Option type="smileyNeutral" text="Nevím"/>
-        <Option type="smileyNo" text="Spíše nesouhlasím"/>
-        <Option type="smileyStrongNo" text="Nesouhlasím"/>
+        <Option type="smileyStrongYes" text="Souhlasím" onSelected={handleSelect}/>
+        <Option type="smileyYes" text="Spíše souhlasím" onSelected={handleSelect}/>
+        <Option type="smileyNeutral" text="Nevím" onSelected={handleSelect}/>
+        <Option type="smileyNo" text="Spíše nesouhlasím" onSelected={handleSelect}/>
+        <Option type="smileyStrongNo" text="Nesouhlasím" onSelected={handleSelect}/>
       </div>
     </div>
-  );
+  )
 };
